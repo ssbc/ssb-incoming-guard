448cc020321c0771d681855a1a5df502d5ae754a
Anders Rune Jensen <arj03@protonmail.ch>
2018-09-13 10:49:47 +0200

    Incorperate changes from @cel

--- index.js
+++ index.js
@@ -9,6 +9,35 @@ exports.init = function (ssb, config) {
 
   ssb.emit('log:info', ['SBOT', 'incoming guard init'])
 
+  if (!ssb.friends || !ssb.friends.hopStream) {
+    ssb.emit('log:error', ['SBOT', 'missing sbot.friends.hopStream'])
+    return
+  }
+
+  var cbs = []
+  var dists = {}
+  function gotHops(data) {
+    for(var k in data) {
+      dists[k] = data[k]
+    }
+  }
+  pull(
+    ssb.friends.hopStream({live: false, old: true}),
+    pull.drain(gotHops, function (err) {
+      if (err) return console.trace(err)
+      while (cbs.length) cbs.shift()()
+      cbs = null
+    })
+  )
+  pull(
+    ssb.friends.hopStream({live: true, old: false}),
+    pull.drain(gotHops)
+  )
+  function onReady(fn) {
+    if (cbs) cbs.push(fn)
+    else fn()
+  }
+
   ssb.auth.hook(function (fn, args) {
     var pubkey = args[0], cb = args[1]
 
@@ -16,22 +45,17 @@ exports.init = function (ssb, config) {
     fn(pubkey, function (err, auth) {
       if(err||auth) return cb(err, auth)
 
-      var serverHops = config.friends && config.friends.hops || 3
-      if(ssb.friends) {
-        pull(
-          ssb.friends.hopStream(),
-          pull.drain(s => {
-            var hops = s[pubkey]
-            if (hops == undefined || hops > serverHops) {
-              ssb.emit('log:info', ['SBOT', 'connection outside hops, auth fail: ', hops])
-              cb(null, { allow: [], deny: null })
-            } else {
-              ssb.emit('log:info', ['SBOT', 'connection inside hops, auth ok: ', hops, pubkey])
-              cb(null, true)
-            }
-          })
-        )
-      }
+      onReady(function () {
+        var serverHops = config.friends && config.friends.hops || 3
+        var hops = dists[pubkey]
+        if (hops == null || hops > serverHops) {
+          ssb.emit('log:info', ['SBOT', 'connection outside hops, auth fail: ', hops])
+          cb(new Error('connection outside hops: ' + pubkey + ' hops: ' + hops))
+        } else {
+          ssb.emit('log:info', ['SBOT', 'connection inside hops, auth ok: ', hops, pubkey])
+          cb(null, false)
+        }
+      })
     })
   })
   
